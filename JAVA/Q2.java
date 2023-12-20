28fc87fdb54a19814496d92a97e5f5c9d3c47639
Subhajyoti Prusty <70449522+subhajyoti-prusty@users.noreply.github.com>
2023-12-20 09:10:58 +0530

    Add files via upload

---
+++ JAVA/Q2.java
@@ -0,0 +1,22 @@
+package HackerRank_Questions.JAVA.src;
+import java.util.*;
+import java.io.*;
+
+class Q2{
+    public static void main(String []argh){
+        Scanner in = new Scanner(System.in);
+        int t=in.nextInt();
+        for(int i=0;i<t;i++){
+            int a = in.nextInt();
+            int b = in.nextInt();
+            int n = in.nextInt();
+            int calc = a;
+            for (int j = 0; j < n; j++) {
+                calc+=(Math.pow(2,j)*b);
+                System.out.print(calc+" ");
+            }
+            System.out.println();
+        }
+        in.close();
+    }
+}
