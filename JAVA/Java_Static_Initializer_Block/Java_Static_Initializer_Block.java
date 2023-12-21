841287cad68ece3b3fb4a157e064ae36c7b2f7cc
Subhajyoti Prusty <70449522+subhajyoti-prusty@users.noreply.github.com>
2023-12-21 21:56:31 +0530

    Add files via upload

---
+++ JAVA/Java_Static_Initializer_Block/Java_Static_Initializer_Block.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Java_Static_Initializer_Block {
+static int B, H; 
+static boolean flag = true; 
+static Scanner Scn = new Scanner(System.in);
+static {
+B = Scn.nextInt();
+H = Scn.nextInt();   
+      
+if (B<=0 || H<=0) {
+System.out.println("java.lang.Exception: Breadth and height must be positive");            
+flag = false;        
+}
+}
+
+public static void main(String[] args){
+		if(flag){
+			int area=B*H;
+			System.out.print(area);
+		}
+		
+	}//end of main
+
+}//end of class
